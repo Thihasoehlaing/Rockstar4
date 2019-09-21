7e007a514f430a276a32bdd013b27877e0e26230
Thiha <thihasoehlaing329@gmail.com>
2019-09-21 14:51:32 +0630

    material-ui headerjs

--- todo-react/src/Header.js
+++ todo-react/src/Header.js
@@ -1,4 +1,16 @@
 import React from "react";
+//import AppBar from "@material-ui/core/AppBar";
+
+import{
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    Badge,
+} from "@material-ui/core";
+
+import DoneAllIcon from "@material-ui/icons/DoneAll";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const styles = {
     header:{
@@ -10,10 +22,22 @@ const styles = {
     }
 };
 
-class Header extends React.Component{
-    render(){
-        return(
-            <h1 style={styles.header}>Todo React({this.props.count})</h1>
+class Header extends React.Component {
+    render() {
+        return (
+            <AppBar position="static">
+                <Toolbar>
+                    <Badge badgeContent={this.props.count} color="secondary">
+                        <DoneAllIcon />
+                    </Badge>
+                    <Typography variant="h6" style={styles.title}>
+                        Todo List
+                    </Typography>
+                    <IconButton color="inherit">
+                        <MoreVertIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
         );
     }
 }
